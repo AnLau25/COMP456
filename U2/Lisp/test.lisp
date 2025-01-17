3c7935fffd856dc3c2c11d180de9ea7a74f1265b
AnLau25 <avald024@uottawa.ca>
2025-01-17 11:11:33 -0500

    up

--- U2/Lisp/test.lisp
+++ U2/Lisp/test.lisp
@@ -1,1 +1,7 @@
-(format t "Hello world~%")
+(defun my_member (x list)
+  (loop for elm in list
+        when (equal x elm) do (return (loop for rest in (member x list) do (format t "~a " rest)))
+        finally (return nil)))
+
+
+(my_member 7 '(5 2 7 8 6))
