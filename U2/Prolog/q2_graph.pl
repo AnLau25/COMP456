dc2e1077387f8f480522a13f142dba257a90671c
AnLau25 <avald024@uottawa.ca>
2025-02-06 11:45:03 -0500

    q2_done_q4_togo

---
+++ U2/Prolog/q2_graph.pl
@@ -0,0 +1,26 @@
+%Test graph
+edge(a, b).
+edge(a, c).
+edge(b, c).
+edge(b, d).
+edge(b, e).
+edge(c, d).
+edge(d, f).
+edge(e, h).
+edge(e, g).
+edge(f, h).
+edge(f, g).
+
+bi_direction(X, Y):- edge(X, Y).
+bi_direction(Y, X):- edge(X, Y).
+
+findPath(Start_node, Goal_node) :- 
+    path(Start_node, Goal_node, [Start_node]).
+
+path(Start_node, Goal_node, L):- bi_direction(Start_node, Goal_node),
+                                 reverse([Goal_node| L], R), write('Path: '), write(R), nl.
+
+path(Current_node, Goal_node, L) :- bi_direction(Current_node, Z), Z \= Goal_node, \+ member(Z, L), path(Z, Goal_node, [Z | L]).
+
+
+
--- U2/Prolog/q3_graph_weighted.pl
+++ U2/Prolog/q3_graph_weighted.pl
@@ -13,5 +13,4 @@ path([(Distance, Goal_node, L)| _], Goal_node, Distance, L).
 path([(S, Current_node, P)| R], Goal_node, Distance_total, L) :- findall((Z, N, [N|P]), (edge(Current_node, N, W), \+ member(N, P), Z is S + W), Children), 
                                                                  append(R, Children, Q), sort(Q, Sorted_Q), path(Sorted_Q, Goal_node, Distance_total, L).
 
-member(X, [X | _]).
-member(X, [_ | T]) :- member(X, T).
+
