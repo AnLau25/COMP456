4645b8203f520c7dade40fa345902083ce73c1fc
AnLau25 <avald024@uottawa.ca>
2025-02-25 11:12:55 -0500

    it prints one

--- U2/Lisp/q4_FWGC.lisp
+++ U2/Lisp/q4_FWGC.lisp
@@ -52,14 +52,32 @@
                                (opp (choux state)))))
     (t nil)))
 
-(defun path (state goal has_been has_found)
+(defun path_DFS (state goal has_been has_found)
   (cond ((null state) has_found) 
         ((equal state goal) (cons (reverse (cons state has_been)) has_found)) 
         ((member state has_been :test #'equal) has_found)
-        (t (append (path (takes_self state) goal (cons state has_been) has_found)
-                   (path (takes_wolf state) goal (cons state has_been) has_found)
-                   (path (takes_goat state) goal (cons state has_been) has_found)
-                   (path (takes_choux state) goal (cons state has_been) has_found)))))
+        (t (append (path_DFS (takes_self state) goal (cons state has_been) has_found)
+                   (path_DFS (takes_wolf state) goal (cons state has_been) has_found)
+                   (path_DFS (takes_goat state) goal (cons state has_been) has_found)
+                   (path_DFS (takes_choux state) goal (cons state has_been) has_found)))))
+
+(defun path_BFS (state goal has_been)
+  (let ((queue (list (list state)))  ; Queue initialized with start state path
+        (has_found nil))              ; To collect all found paths
+    (loop while queue do
+      (let* ((current-path (pop queue))  
+             (current-state (car (last current-path))))
+        (unless (member current-state has_been :test #'equal)
+          (push current-state has_been) 
+          (if (equal current-state goal)
+              (push (reverse current-path) has_found) 
+              (dolist (next-state (list (takes_self current-state)
+                                        (takes_wolf current-state)
+                                        (takes_goat current-state)
+                                        (takes_choux current-state)))
+                (when next-state 
+                  (push (append current-path (list next-state)) queue)))))))
+    has_found)) ; Return all found paths
 
 
 (defun print_found (found)
@@ -68,8 +86,13 @@
         do (format t "~A: ~A~%" i path)))
 
 (defun fwgc (state goal)
-    (let ((found (path state goal nil nil)))
-    (print_found found)))
+    (format t "DFS:~%")
+    (let ((found1 (path_DFS state goal nil nil)))
+    (print_found found1))
+    
+    (format t "BFS:~%")
+    (let ((found2 (path_BFS state goal nil)))
+    (print_found found2)))
 
 (fwgc (define_state 'e 'e 'e 'e) (define_state 'w 'w 'w 'w))
 
