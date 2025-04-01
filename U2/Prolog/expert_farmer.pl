abdd9f132cd91a750501ba1e5a73b91036fda89c
AnLau25 <avald024@uottawa.ca>
2025-04-01 13:06:07 -0400

    weaccdiditletsgo!

--- U2/Prolog/expert_farmer.pl
+++ U2/Prolog/expert_farmer.pl
@@ -1,12 +1,8 @@
-%Get_world
+% Get_world
 :- dynamic fact/1, rule/1, askable/2.
 consult('C:/Users/User/Documents/A_UNI/YOff/Atha/COMP456/U2/Prolog/knowledge_farmer.pl').
 
-
-
-
-
-%Handle_communication_with_user
+% Handle_communication_with_user
 abbrev(n, no).
 abbrev(y, yes).
 abbrev(q, quit).
@@ -17,32 +13,7 @@ print_help:-
     writeln('   no.   (n) - to disagree'), nl,
     writeln('   yes.  (y) - to agree'), nl,
     writeln('   quit. (q) - to leave system'), nl,
-    writeln('   help.     - to have Nano remind you the commands'), nl. 
-
-interact(Question, ExplainQ, Invalid_Ans, Arg):-  
-    \+ member(Invalid_Ans, [help, yes, no, quit]),  
-    writeln("Nano's commands only include (n)o, (y)es, (q)uit and help."), nl,  
-    ask(ExplainQ, Ans),  
-    interact(Question, ExplainQ, Ans, Arg).  
-
-interact(Question, ExplainQ, help, Arg):-
-    print_help,
-    ask(ExplainQ, Ans),
-    interact(Question, ExplainQ, Ans, Arg).
-
-interact(_, _, quit, _):- writeln('Bye!'), halt.
-
-interact(start, _, no, _):- interact(_, _, quit, _).
-interact(start, _, yes, One_Soln):- search_Engine(One_Soln), writeln('Got the solutions!'), nl.
-
-interact(see_sln, _, no, _).
-interact(see_sln, _, yes, One_Soln):- writeln('Here is your first solution:'), nl, write_Soln(One_Soln).
-
-interact(explain, _, no, _).
-interact(explain, _, yes, Moves):- write_Explain(Moves), write('Goal reached!'), nl.
-
-interact(see_othr, _, no, _).
-interact(see_othr, _, yes, T):- writeln('Here is an alternative solution:'), nl, write_Soln(T).
+    writeln('   help.     - to have Nano remind you the commands'), nl.
 
 ask(Question, Ans):-
     writeln(Question),
@@ -50,60 +21,62 @@ ask(Question, Ans):-
     abbrev(A, Ans),
     nl, !.
 
+interact(_, _, quit, _):- writeln('Bye!'), halt.
+interact(start, _, no, _):- interact(_, _, quit, _).
+interact(start, _, yes, One_Soln):- search_Engine(One_Soln), writeln('Got the solutions!'), nl.
 
+interact(see_sln, _, no, _).
+interact(see_sln, _, yes, One_Soln):- writeln('Here is your first solution:'), nl, write_Soln(One_Soln).
 
+interact(explain, _, no, _).
+interact(explain, _, yes, Moves):- write_Explain(Moves), writeln('Goal reached!'), nl.
 
+interact(see_othr, _, no, _).
+interact(see_othr, _, yes, []):- nl, write_Soln([]).
+interact(see_othr, _, yes, T):- writeln('Here is an alternative solution:'), nl, write_Soln(T).
 
-%Search_algorithm_definition
+% Search_algorithm_definition
 search_Engine(One_Soln):-  
     fact(init_State(I)),  
-    findall((Soln, Moves),  
-            dfs([I], ['Initial state'], [], Soln, Moves),  
-            Solutions),  
+    setof((Soln, Moves), dfs([I], ['Initial state'], [], Soln, Moves), Solutions),  
     (Solutions \= [] -> One_Soln = Solutions ;  
      writeln("No solutions found!"), fail).  
 
-dfs(Path, Inv_Moves, _, Soln, Moves):-
+dfs(Path, Inv_Moves, Visited, Soln, Moves):-
     [H|_] = Path, 
     fact(goal_State(H)),
     reverse(Path, Soln),
     reverse(Inv_Moves, Moves).
 
-dfs(Path, Inv_Moves, Visited, Inv_Soln, Moves):-  
+dfs(Path, Inv_Moves, Visited, Soln, Moves):-  
     [H|_] = Path,  
     rule(move(H, Nstate, Current)),  
-    \+ member(Nstate, Visited),  
+    \+ member(Nstate, Path),  
     fact(is_safe(Nstate)),  
-    fact(is_safe_explained(Nstate, Inv_New_Explained)),  
+    fact(is_safe_explained(H, Inv_New_Explained)),  
 
     reverse(Inv_New_Explained, New_Explained),  
     New_Inv_Moves = [New_Explained|Inv_Moves],  
 
-    dfs([Nstate|Path], [Current|New_Inv_Moves], [H|Visited], Inv_Soln, Moves).  
+    dfs([Nstate|Path], [Current|New_Inv_Moves], [H|Visited], Soln, Moves).  
 
-
-
-
-
-
-
-
-%Solution_display
-writelist([]).  %Well_See
+% Solution_display
+writelist([]).  
 writelist([H]):- write(H), write('.'), nl, nl.  
 writelist([H | T]):- write(H), write(','), nl, writelist(T).  
 
-write_Explain([H|[T1|T2]]):-
-    write(H), write(','), nl, nl,
-    writeln('So, since I saw:'),
-    writelist(T1),
-    writeln('I chose:'),
-    write_Explain(T2).
+write_Explain([H|[]]):- writeln(H), nl.
+write_Explain([Move, Explanation | RestMoves]) :-
+    writeln('So I chose:'),
+    writeln(Move), nl,
+    writeln('I saw:'),
+    writelist(Explanation),
+    write_Explain(RestMoves).
 
 write_Soln_inWords([H|[]]):- write(H), nl, nl.
 write_Soln_inWords([H|[_|T2]]):- write(H), write(','), nl, nl, write_Soln_inWords(T2).
 
-write_Soln([]):- writeln('There you go, those were all the solutions found'), nl.
+write_Soln([]):- writeln('Those were all the solutions found'), nl.
 write_Soln([(Soln, Moves)|T]):- 
     writeln('Sides of the river:'), 
     writeln('We have e = east and w = west for the two shores'), nl,
@@ -124,27 +97,19 @@ write_Soln([(Soln, Moves)|T]):-
     ask(Question, Ans),
     interact(see_othr, Question, Ans, T).
 
-
-
-
-
-%Expert_coms_I_named_it_Nano
+% Expert_coms_I_named_it_Nano
 nano:-
     nl, writeln("Hi! I'm Nano, your puzzle expert system."),
     nl, writeln("Let's solve the Farmer, Wolf, Goat, Cabbage problem together!"),
     print_help,
 
-    %Start_search?
     askable(start, Quest1),
     ask(Quest1, Ans1),
     interact(start, Quest1, Ans1, One_Soln),
 
-    %See_firs_solution?
     askable(see_sln, Quest2),
     ask(Quest2, Ans2),
     interact(see_sln, Quest2, Ans2, One_Soln),
 
-    %See_more_solutions?
     writeln('Well, that was fun, see you next time.'),
     nl, writeln('Nano, out!').
-
