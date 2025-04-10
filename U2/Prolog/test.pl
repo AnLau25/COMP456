8a232ccf0452bc1444525b65fe745f307375e32d
AnLau25 <avald024@uottawa.ca>
2025-04-09 22:13:18 -0400

    IYielded

--- U2/Prolog/test.pl
+++ U2/Prolog/test.pl
@@ -9,4 +9,4 @@ writelist([H | T]):- write(H), nl, writelist(T).
 rev_writelist([]).
 rev_writelist([H | T]):- rev_writelist(T), write(H), nl.
 
-%This_is_a_comment
+%This_is_a_comment_that_I_will_keep_stretching
